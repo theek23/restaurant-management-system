c907c1ce1761b0e27f4753b21b32f9f8937cd2e9
theek23 <theekshanadesilva28@gmail.com>
2023-11-06 02:54:22 +0530

    Updating not added file to git

--- src/main/java/lk/rms/controller/DrinksFormController.java
+++ src/main/java/lk/rms/controller/DrinksFormController.java
@@ -1,4 +1,25 @@
 package lk.rms.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import lombok.SneakyThrows;
+
 public class DrinksFormController {
+    public void softDrinksBtnsOnAction(ActionEvent actionEvent) {
+
+    }
+
+    @SneakyThrows
+    public void hotDrinksBtnsOnAction(ActionEvent actionEvent) {
+        Stage stage = new Stage();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../../../view/qtyForm.fxml"))));
+        stage.setTitle("ADD QTY");
+        stage.setResizable(false);
+        stage.getIcons().add(new Image("asset/Logo.png"));
+        stage.centerOnScreen();
+        stage.show();
+    }
 }
