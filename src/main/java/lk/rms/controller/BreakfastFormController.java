1feb9a05cfd27355664213b1d4e282bced7adae2
theek23 <theekshanadesilva28@gmail.com>
2023-11-05 18:19:08 +0530

    add items updated

--- src/main/java/lk/rms/controller/BreakfastFormController.java
+++ src/main/java/lk/rms/controller/BreakfastFormController.java
@@ -1,9 +1,22 @@
 package lk.rms.controller;
 
+import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
 
 public class BreakfastFormController {
-    public void itemsBtnsOnAction(ActionEvent actionEvent) {
+    public JFXButton itemBtn1,itemBtn2,itemBtn3,itemBtn4,itemBtn5,itemBtn6,itemBtn7,itemBtn8,itemBtn9,itemBtn10,itemBtn11,
+            itemBtn12,itemBtn13,itemBtn14,itemBtn15,itemBtn16,itemBtn17,itemBtn18,itemBtn19,itemBtn20,itemBtn21,itemBtn22,
+            itemBtn23,itemBtn24,itemBtn25,itemBtn26,itemBtn27,itemBtn28,itemBtn29,itemBtn30,itemBtn31,itemBtn32,itemBtn33,
+            itemBtn34,itemBtn35,itemBtn36,itemBtn37,itemBtn38,itemBtn39,itemBtn40,itemBtn41,itemBtn42;
 
+
+    public void itemsBtnsOnAction(ActionEvent actionEvent) {
+        Object source = actionEvent.getSource();
+
+        if (source instanceof JFXButton) {
+            JFXButton button = (JFXButton) source;
+            String buttonId = button.getId();
+            System.out.println("Button ID: " + buttonId);
+        }
     }
 }
--- src/main/java/lk/rms/controller/DinnerFormController.java
+++ src/main/java/lk/rms/controller/DinnerFormController.java
@@ -1,4 +1,15 @@
 package lk.rms.controller;
 
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+
 public class DinnerFormController {
+    public JFXButton itemBtn1,itemBtn2,itemBtn3,itemBtn4,itemBtn5,itemBtn6,itemBtn7,itemBtn8,itemBtn9,itemBtn10,itemBtn11,
+            itemBtn12,itemBtn13,itemBtn14,itemBtn15,itemBtn16,itemBtn17,itemBtn18,itemBtn19,itemBtn20,itemBtn21,itemBtn22,
+            itemBtn23,itemBtn24,itemBtn25,itemBtn26,itemBtn27,itemBtn28,itemBtn29,itemBtn30,itemBtn31,itemBtn32,itemBtn33,
+            itemBtn34,itemBtn35,itemBtn36,itemBtn37,itemBtn38,itemBtn39,itemBtn40,itemBtn41,itemBtn42;
+
+    public void itemsBtnsOnAction(ActionEvent actionEvent) {
+
+    }
 }
